22fc7b07b9997bd9296c9794c8f511668beee67c
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-04-28 15:23:08 +0000

    bucle de las cards

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,19 @@ import MyJumbotron from "./myjumbotron.jsx";
 import { MyNavbar } from "./mynavbar.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { MyCard } from "./mycard.jsx";
+import { MyCardLoop, MyCard } from "./mycard.jsx";
 
 //create your first component
 export function Home() {
+	let loop = [];
+	for (let index = 0; index < 4; index++) {
+		loop.push(MyCard());
+	}
 	return (
 		<div className="text-center mt-5">
 			<MyNavbar />
 			<MyJumbotron />
-			<div className="cardbox">
-				<MyCard />
-				<MyCard />
-				<MyCard />
-				<MyCard />
-			</div>
+			<div className="cardbox">{loop}</div>
 		</div>
 	);
 }
